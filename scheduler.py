5ee758703b493dfa2b94e55cb9be906a4f6f4c5d
clinton <clintonletsoela724c@gmail.com>
2022-10-26 16:28:30 +0200

    changed

--- scheduler.py
+++ scheduler.py
@@ -5,7 +5,7 @@ from apscheduler.schedulers.twisted import TwistedScheduler
 from twisted.internet import reactor
 
 def send_request():
-    requests.post("https://tranquil-hamlet-30660.herokuapp.com/schedule.json", data={
+    requests.post("https://boiling-shore-22216.herokuapp.com/schedule.json", data={
         "project": "cryptocurrency",
         "spider": "getUrls"
     })
@@ -14,7 +14,7 @@ if __name__ == "__main__":
     subprocess.run("scrapyd-deploy", shell=True, universal_newlines=True)
     scheduler = TwistedScheduler(timezone=pytz.timezone('Africa/Johannesburg'))
     # cron trigger that schedules job every every 20 minutues on weekdays
-    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour='16', minute='21')
+    scheduler.add_job(send_request, 'cron', day_of_week='mon-sun', hour='16', minute='30')
     # start the scheduler
     scheduler.start()
     reactor.run()
